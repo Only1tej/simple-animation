866fc57e2e52c5d346e2ac0e08222aa6cf8e827b
only1tej <tijanienny3@gmail.com>
2023-08-07 09:49:16 -0700

    Gesture in both x & y axis

--- src/components/Gesture.js
+++ src/components/Gesture.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
-import { useGesture } from "@use-gesture/react";
+import { useGesture, useDrag } from "@use-gesture/react";
 
 const Gesture = () => {
-  const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }));
+  const [{ xy }, api] = useSpring(() => ({ xy: [0, 0] }));
 
-  const bind = useGesture(({ down, delta }) => {
-    set({ xy: down ? delta : [0, 0] });
+  const bind = useDrag(({ down, delta }) => {
+    api.start({ xy: down ? delta : [0, 0] });
   });
   return (
     <animated.div
       style={{
-        transform: xy.to((x, y) => `translate3d(${x}px, ${y}px, 0)`),
+        transform: xy.interpolate((x, y) => `translate3d(${x}px, ${y}px, 0)`),
       }}
       {...bind()}
       className="box"
