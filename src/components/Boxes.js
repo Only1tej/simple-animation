7705e93e53c8504104b71f3193ba02d519574f74
only1tej <tijanienny3@gmail.com>
2023-08-08 12:26:52 -0700

    useChain

--- src/components/Boxes.js
+++ src/components/Boxes.js
@@ -1,26 +1,44 @@
 import React, { useState, useRef } from "react";
-import { animated, useTrail, useSpring } from "react-spring";
+import {
+  animated,
+  useTrail,
+  useSpring,
+  useSpringRef,
+  useChain,
+} from "react-spring";
 
 const items = [0, 1, 2, 3, 4];
 // const items = [0.5, 0.3, 0.2, 0.7, 1]; (items used as the vale for the 'to' opacity)
 
 const Boxes = () => {
   const [on, toggle] = useState(false);
-  const springRef = useRef();
-  const spring = useSpring({
-    size: on ? "100%" : "20%",
+  const springRef = useSpringRef();
+  const { size } = useSpring({
+    ref: springRef,
+    from: { size: "20%" },
+    to: { size: on ? "100%" : "20%" },
   });
-  const transitionRef = useRef();
+
+  const transitionRef = useSpringRef();
   const trail = useTrail(items.length, {
     ref: transitionRef,
-    opacity: on ? 1 : 0,
-    transform: on ? "scale(1)" : "scale(0)",
+    from: { opacity: 0, transform: "scale(0)" },
+    to: { opacity: on ? 1 : 0, transform: on ? "scale(1)" : "scale(0)" },
   });
+
+  useChain(on ? [springRef, transitionRef] : [transitionRef, springRef]);
+
   return (
-    <div className="boxes-grid-two" onClick={() => toggle(!on)}>
-      {trail.map((animation) => (
-        <animated.div className="box-two" style={animation} />
-      ))}
+    <div className="full-height">
+      <animated.div
+        style={{ width: size, height: size }}
+        className="boxes-grid-two"
+        onClick={() => toggle(!on)}
+      >
+        {trail.map((animation, index) => (
+          <animated.div className="box-two" style={animation} key={index} />
+        ))}
+      </animated.div>
     </div>
   );
 };
